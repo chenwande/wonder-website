ad792b947c2708e3586ef6bbd4e7d22b47dd20fc
陈万德 <chenwand@cyg.com>
2025-05-07 16:30:01 +0800

    Updates

---
+++ meta__canvas.d4b9c23c.async.js
@@ -0,0 +1,106 @@
+"use strict";(self.webpackChunkwonder=self.webpackChunkwonder||[]).push([[392],{66768:function(u,a,t){var m;t.r(a),t.d(a,{demos:function(){return r}});var c=t(90228),p=t.n(c),x=t(87999),d=t.n(x),_=t(75271),v=t(61384),h=t(74908),r={"src-canvas-demo-demo":{component:_.memo(_.lazy(function(){return Promise.all([t.e(847),t.e(433)]).then(t.bind(t,26878))})),asset:{type:"BLOCK",id:"src-canvas-demo-demo",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:t(16478).Z},react:{type:"NPM",value:"18.3.1"},"./utils.ts":{type:"FILE",value:t(65537).Z}},entry:"index.tsx"},context:{"./utils.ts":h,react:m||(m=t.t(_,2)),"/Users/chenwande/test/wonder/src/canvas/utils.ts":h},renderOpts:{compile:function(){var e=d()(p()().mark(function n(){var s,l=arguments;return p()().wrap(function(o){for(;;)switch(o.prev=o.next){case 0:return o.next=2,t.e(223).then(t.bind(t,61223));case 2:return o.abrupt("return",(s=o.sent).default.apply(s,l));case 3:case"end":return o.stop()}},n)}));function i(){return e.apply(this,arguments)}return i}()}}}},74908:function(u,a,t){t.r(a),t.d(a,{Graph:function(){return v},Point:function(){return _},getRandom:function(){return d}});var m=t(25298),c=t.n(m),p=t(17069),x=t.n(p),d=function(r,e){return Math.random()*(e-r)+r},_=function(){function h(r,e){c()(this,h),this.ctx=r,this.canvas=e,this.r=4,this.x=d(0,e.width-this.r/2),this.y=d(0,e.height-this.r/2),this.xSpeed=d(-50,50),this.ySpeed=d(-50,50),this.lastDrawTime=null}return x()(h,[{key:"draw",value:function(){if(!this.lastDrawTime)this.lastDrawTime=Date.now();else{var e=Date.now(),i=(e-this.lastDrawTime)/1e3,n=this.x+this.xSpeed*i,s=this.y+this.ySpeed*i;n<0?(n=0,this.xSpeed=-this.xSpeed):n>this.canvas.width-this.r/2&&(n=this.canvas.width-this.r/2,this.xSpeed=-this.xSpeed),s<0?(s=0,this.ySpeed=-this.ySpeed):s>this.canvas.height-this.r/2&&(s=this.canvas.height-this.r/2,this.ySpeed=-this.ySpeed),this.x=n,this.y=s,this.lastDrawTime=e}this.ctx.beginPath(),this.ctx.arc(this.x,this.y,this.r,0,Math.PI*2),this.ctx.fillStyle="rgb(200, 200, 200)",this.ctx.fill()}}]),h}(),v=function(){function h(r,e){var i=this,n=arguments.length>2&&arguments[2]!==void 0?arguments[2]:40,s=arguments.length>3&&arguments[3]!==void 0?arguments[3]:300;c()(this,h),this.canvas=e,this.ctx=r,this.points=Array(n).fill(0).map(function(){return new _(i.ctx,i.canvas)}),this.maxDistance=s,this.draw()}return x()(h,[{key:"draw",value:function(){var e=this;requestAnimationFrame(function(){e.draw()}),this.ctx.clearRect(0,0,this.canvas.width,this.canvas.height);for(var i=0;i<this.points.length;i++){var n=this.points[i];n.draw();for(var s=i+1;s<this.points.length;s++){var l=this.points[s],f=Math.sqrt(Math.pow(n.x-l.x,2)+Math.pow(n.y-l.y,2)),o=1-f/this.maxDistance;this.ctx.beginPath(),this.ctx.moveTo(n.x,n.y),this.ctx.lineTo(l.x,l.y),this.ctx.strokeStyle="rgba(200, 200, 200, ".concat(o,")"),this.ctx.stroke()}}}}]),h}()},11515:function(u,a,t){t.r(a),t.d(a,{texts:function(){return c}});var m=t(61384);const c=[]},16478:function(u,a){a.Z=`import React, { useEffect } from 'react';
+import { Graph } from './utils';
+
+const Demo = () => {
+  useEffect(() => {
+    const canvas = document.getElementById('canvas-demo') as HTMLCanvasElement;
+    canvas.width = devicePixelRatio * canvas.clientWidth;
+    canvas.height = devicePixelRatio * 500;
+    const ctx = canvas.getContext('2d')!;
+
+    new Graph(ctx, canvas);
+  }, []);
+
+  return (
+    <canvas
+      id="canvas-demo"
+      style={{ width: '100%', height: '100%', backgroundColor: '#222' }}
+    ></canvas>
+  );
+};
+
+export default Demo;
+`},65537:function(u,a){a.Z=`// @ts-nocheck
+export const getRandom = (min, max) => {
+  return Math.random() * (max - min) + min;
+};
+
+export class Point {
+  constructor(ctx, canvas) {
+    this.ctx = ctx;
+    this.canvas = canvas;
+    this.r = 4; // \u5706\u7684\u534A\u5F84
+    this.x = getRandom(0, canvas.width - this.r / 2); // \u5706\u5FC3\u7684x\u5750\u6807
+    this.y = getRandom(0, canvas.height - this.r / 2); // \u5706\u5FC3\u7684y\u5750\u6807
+    this.xSpeed = getRandom(-50, 50); // \u5706\u5FC3x\u8F74\u7684\u901F\u5EA6
+    this.ySpeed = getRandom(-50, 50); // \u5706\u5FC3x\u8F74\u7684\u901F\u5EA6
+    this.lastDrawTime = null;
+  }
+  draw() {
+    if (!this.lastDrawTime) {
+      this.lastDrawTime = Date.now();
+    } else {
+      const now = Date.now();
+      const delta = (now - this.lastDrawTime) / 1000;
+      let x = this.x + this.xSpeed * delta;
+      let y = this.y + this.ySpeed * delta;
+      if (x < 0) {
+        x = 0;
+        this.xSpeed = -this.xSpeed;
+      } else if (x > this.canvas.width - this.r / 2) {
+        x = this.canvas.width - this.r / 2;
+        this.xSpeed = -this.xSpeed;
+      }
+      if (y < 0) {
+        y = 0;
+        this.ySpeed = -this.ySpeed;
+      } else if (y > this.canvas.height - this.r / 2) {
+        y = this.canvas.height - this.r / 2;
+        this.ySpeed = -this.ySpeed;
+      }
+      this.x = x;
+      this.y = y;
+      this.lastDrawTime = now;
+    }
+
+    this.ctx.beginPath();
+    this.ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2);
+    this.ctx.fillStyle = 'rgb(200, 200, 200)';
+    this.ctx.fill();
+  }
+}
+
+export class Graph {
+  constructor(ctx, canvas, pointNumber = 40, maxDistance = 300) {
+    this.canvas = canvas;
+    this.ctx = ctx;
+    this.points = Array(pointNumber)
+      .fill(0)
+      .map(() => new Point(this.ctx, this.canvas));
+    this.maxDistance = maxDistance;
+    this.draw();
+  }
+  draw() {
+    requestAnimationFrame(() => {
+      this.draw();
+    });
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    for (let i = 0; i < this.points.length; i++) {
+      const p1 = this.points[i];
+      p1.draw();
+      for (let j = i + 1; j < this.points.length; j++) {
+        const p2 = this.points[j];
+        const distance = Math.sqrt(
+          Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2),
+        );
+        const opacity = 1 - distance / this.maxDistance;
+        this.ctx.beginPath();
+        this.ctx.moveTo(p1.x, p1.y);
+        this.ctx.lineTo(p2.x, p2.y);
+        this.ctx.strokeStyle = \`rgba(200, 200, 200, \${opacity})\`;
+        this.ctx.stroke();
+      }
+    }
+  }
+}
+`}}]);
--- meta__large-file-upload.03009334.async.js
+++ meta__large-file-upload.03009334.async.js
@@ -1,5 +1,4 @@
-"use strict";(self.webpackChunkwonder=self.webpackChunkwonder||[]).push([[863],{10889:function(_,n,e){var u;e.r(n),e.d(n,{demos:function(){return v}});var d=e(90228),m=e.n(d),p=e(87999),g=e.n(p),o=e(75271),a=e(31383),s=e(77880),t=e(34847),r=e.n(t),v={"src-large-file-upload-demo-demo":{component:o.memo(o.lazy(function(){return Promise.all([e.e(847),e.e(433)]).then(e.bind(e,16840))})),asset:{type:"BLOCK",id:"src-large-file-upload-demo-demo",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:e(96447).Z},react:{type:"NPM",value:"18.3.1"},"./utils.ts":{type:"FILE",value:e(39868).Z},"spark-md5":{type:"NPM",value:"3.0.2"}},entry:"index.tsx"},context:{"./utils.ts":s,react:u||(u=e.t(o,2)),"/Users/chenwande/project/wonder/src/large-file-upload/utils.ts":s,"spark-md5":t},renderOpts:{compile:function(){var i=g()(m()().mark(function f(){var c,h=arguments;return m()().wrap(function(l){for(;;)switch(l.prev=l.next){case 0:return l.next=2,e.e(147).then(e.bind(e,14147));case 2:return l.abrupt("return",(c=l.sent).default.apply(c,h));case 3:case"end":return l.stop()}},f)}));function E(){return i.apply(this,arguments)}return E}()}}}},77880:function(_,n,e){e.r(n),e.d(n,{createFileChunks:function(){return m},getFileHash:function(){return p},getFileHashByWebWorker:function(){return g}});var u=e(34847),d=e.n(u),m=function(a){for(var s=arguments.length>1&&arguments[1]!==void 0?arguments[1]:10485760,t=[],r=0;r<a.size;r+=s)t.push(a.slice(r,r+s));return t},p=function(a){return new Promise(function(s){var t=new(d()),r=function v(i){if(i>=a.length){s(t.end());return}var E=a[i],f=new FileReader;f.onload=function(c){var h;if((h=c.target)!==null&&h!==void 0&&h.result){var k=c.target.result;t.append(k),v(i+1)}},f.readAsArrayBuffer(E)};r(0)})},g=function(a){return new Promise(function(s){var t=new Worker(new URL(e.p+e.u(16),e.b),{type:void 0});t.postMessage({chunks:a}),t.onmessage=function(r){s(r.data)}})}},3813:function(_,n,e){e.r(n),e.d(n,{texts:function(){return d}});var u=e(31383);const d=[]},96447:function(_,n){n.Z=`// @ts-nocheck
-import React, { useState } from 'react';
+"use strict";(self.webpackChunkwonder=self.webpackChunkwonder||[]).push([[863],{99160:function(_,n,e){var u;e.r(n),e.d(n,{demos:function(){return v}});var d=e(90228),m=e.n(d),g=e(87999),p=e.n(g),o=e(75271),a=e(91263),s=e(63120),t=e(34847),r=e.n(t),v={"src-large-file-upload-demo-demo":{component:o.memo(o.lazy(function(){return Promise.all([e.e(847),e.e(433)]).then(e.bind(e,30048))})),asset:{type:"BLOCK",id:"src-large-file-upload-demo-demo",refAtomIds:[],dependencies:{"index.tsx":{type:"FILE",value:e(27486).Z},react:{type:"NPM",value:"18.3.1"},"./utils.ts":{type:"FILE",value:e(71397).Z},"spark-md5":{type:"NPM",value:"3.0.2"}},entry:"index.tsx"},context:{"./utils.ts":s,react:u||(u=e.t(o,2)),"/Users/chenwande/test/wonder/src/large-file-upload/utils.ts":s,"spark-md5":t},renderOpts:{compile:function(){var i=p()(m()().mark(function f(){var h,c=arguments;return m()().wrap(function(l){for(;;)switch(l.prev=l.next){case 0:return l.next=2,e.e(223).then(e.bind(e,61223));case 2:return l.abrupt("return",(h=l.sent).default.apply(h,c));case 3:case"end":return l.stop()}},f)}));function E(){return i.apply(this,arguments)}return E}()}}}},63120:function(_,n,e){e.r(n),e.d(n,{createFileChunks:function(){return m},getFileHash:function(){return g},getFileHashByWebWorker:function(){return p}});var u=e(34847),d=e.n(u),m=function(a){for(var s=arguments.length>1&&arguments[1]!==void 0?arguments[1]:10485760,t=[],r=0;r<a.size;r+=s)t.push(a.slice(r,r+s));return t},g=function(a){return new Promise(function(s){var t=new(d()),r=function v(i){if(i>=a.length){s(t.end());return}var E=a[i],f=new FileReader;f.onload=function(h){var c;if((c=h.target)!==null&&c!==void 0&&c.result){var k=h.target.result;t.append(k),v(i+1)}},f.readAsArrayBuffer(E)};r(0)})},p=function(a){return new Promise(function(s){var t=new Worker(new URL(e.p+e.u(27),e.b),{type:void 0});t.postMessage({chunks:a}),t.onmessage=function(r){s(r.data)}})}},2274:function(_,n,e){e.r(n),e.d(n,{texts:function(){return d}});var u=e(91263);const d=[]},27486:function(_,n){n.Z=`import React, { useState } from 'react';
 import { createFileChunks, getFileHash, getFileHashByWebWorker } from './utils';
 
 const Demo = () => {
@@ -37,8 +36,7 @@ const Demo = () => {
 };
 
 export default Demo;
-`},39868:function(_,n){n.Z=`// @ts-nocheck
-import SparkMd5 from 'spark-md5';
+`},71397:function(_,n){n.Z=`import SparkMd5 from 'spark-md5';
 
 export const createFileChunks = (file: File, chunkSize = 10 * 1024 * 1024) => {
   const chunks: Blob[] = [];
@@ -73,6 +71,7 @@ export const getFileHash = (chunks: Blob[]): Promise<string> =>
 
 export const getFileHashByWebWorker = (chunks: Blob[]): Promise<string> => {
   return new Promise((resolve) => {
+    // @ts-ignore
     const worker = new Worker(new URL('./worker.ts', import.meta.url), {
       type: 'module',
     });
